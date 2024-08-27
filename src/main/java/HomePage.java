45d58146ad7029ca64d2d556716364b88bfe294a
Furkan Yıldırım <yildirim.frkn@outlook.com>
2024-08-27 11:43:32 +0300

    search func and locators ok

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -7,12 +7,15 @@ public class HomePage extends BasePage{
     SearchBar searchBar;
     public HomePage(WebDriver driver) {
         super(driver);
+        searchBar = new SearchBar(driver);
     }
 
     public SearchBar SearchBar() {
+
     }
 
     public boolean cartCount() {
+        return false;
     }
 
     public void GoToCart() {
--- src/main/java/SearchBar.java
+++ src/main/java/SearchBar.java
@@ -1,6 +1,18 @@
-public class SearchBar {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchBar extends BasePage{
+
+    By searchBarLocator = By.xpath("//input[@placeholder='Ürün, kategori veya marka ara']");
+
+    By searchbuttonLocator = By.xpath("//div[@class='searchBoxOld-yDJzsIfi_S5gVgoapx6f']");
+
+    public SearchBar(WebDriver driver) {
+        super(driver);
+    }
 
     public void search(String text){
-
+        type(searchBarLocator, text);
+        click(searchbuttonLocator);
     }
 }
